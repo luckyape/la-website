07f19d418fc09531d62aae1138b63017c7455a8f
graham charles <graham@luckyape.com>
2017-11-05 16:09:23 -0800

    fonts update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -52,73 +52,73 @@ dotenv.config();
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
-  .pipe($.eslint())
-  .pipe($.eslint.format())
-  .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
+	gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
+	.pipe($.eslint())
+	.pipe($.eslint.format())
+	.pipe($.if(!browserSync.active, $.eslint.failAfterError()))
 );
 
 // move fonts
 gulp.task('copyfonts', () =>
-  gulp.src('./app/fonts/*/*')
-  .pipe(gulp.dest('./dist/public/fonts'))
+	gulp.src('./app/fonts/*/*')
+	.pipe(gulp.dest('./dist/public/fonts'))
 );
 
 // Optimize images
 gulp.task('images', () =>
-  gulp.src('app/images/**/*')
-  .pipe($.cache($.imagemin({
-    progressive: true,
-    interlaced: true
-  })))
-  .pipe(gulp.dest('dist/public/images'))
-  .pipe($.size({ title: 'images' }))
+	gulp.src('app/images/**/*')
+	.pipe($.cache($.imagemin({
+		progressive: true,
+		interlaced: true
+	})))
+	.pipe(gulp.dest('dist/public/images'))
+	.pipe($.size({ title: 'images' }))
 );
 
 // Copy all files at the root level (app)
 gulp.task('copy', () =>
-  gulp.src([
-    'app/*',
-    '!app/*.html',
-    'node_modules/apache-server-configs/dist/public/.htaccess'
-  ], {
-    dot: true
-  }).pipe(gulp.dest('dist/public'))
-  .pipe($.size({ title: 'copy' }))
+	gulp.src([
+		'app/*',
+		'!app/*.html',
+		'node_modules/apache-server-configs/dist/public/.htaccess'
+	], {
+		dot: true
+	}).pipe(gulp.dest('dist/public'))
+	.pipe($.size({ title: 'copy' }))
 );
 
 // Compile and automatically prefix stylesheets
 gulp.task('styles', () => {
-  const AUTOPREFIXER_BROWSERS = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
-  ];
+	const AUTOPREFIXER_BROWSERS = [
+		'ie >= 10',
+		'ie_mob >= 10',
+		'ff >= 30',
+		'chrome >= 34',
+		'safari >= 7',
+		'opera >= 23',
+		'ios >= 7',
+		'android >= 4.4',
+		'bb >= 10'
+	];
 
-  // For best performance, don't add Sass partials to `gulp.src`
-  return gulp.src([
-      'app/styles/**/*.scss',
-      'app/styles/**/*.css'
-    ])
-    .pipe($.newer('.tmp/styles'))
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      precision: 10
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe(gulp.dest('.tmp/styles'))
-    // Concatenate and minify styles
-    .pipe($.if('*.css', $.cssnano({ minifyFontValues: false, discardUnused: false })))
-    .pipe($.size({ title: 'styles' }))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest('dist/public/styles'))
-    .pipe(gulp.dest('.tmp/styles'));
+	// For best performance, don't add Sass partials to `gulp.src`
+	return gulp.src([
+			'app/styles/**/*.scss',
+			'app/styles/**/*.css'
+		])
+		.pipe($.newer('.tmp/styles'))
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({
+			precision: 10
+		}).on('error', $.sass.logError))
+		.pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+		.pipe(gulp.dest('.tmp/styles'))
+		// Concatenate and minify styles
+		.pipe($.if('*.css', $.cssnano({ minifyFontValues: false, discardUnused: false })))
+		.pipe($.size({ title: 'styles' }))
+		.pipe($.sourcemaps.write('./'))
+		.pipe(gulp.dest('dist/public/styles'))
+		.pipe(gulp.dest('.tmp/styles'));
 });
 
 
@@ -126,81 +126,81 @@ gulp.task('styles', () => {
 // to enable ES2015 support remove the line `"only": "gulpfile.babel.js",` in the
 // `.babelrc` file.
 gulp.task('scripts', () =>
-  gulp.src([
-    // Note: Since we are not using useref in the scripts build pipeline,
-    //       you need to explicitly list your scripts here in the right order
-    //       to be correctly concatenated
-    './app/scripts/main.js',
-    './app/scripts/init.js'
+	gulp.src([
+		// Note: Since we are not using useref in the scripts build pipeline,
+		//       you need to explicitly list your scripts here in the right order
+		//       to be correctly concatenated
+		'./app/scripts/main.js',
+		'./app/scripts/init.js'
 
-    // Other scripts
-  ])
-  .pipe($.newer('.tmp/scripts'))
-  .pipe($.sourcemaps.init())
-  .pipe($.babel())
-  .pipe($.sourcemaps.write())
-  .pipe(gulp.dest('.tmp/scripts'))
-  .pipe($.concat('main.min.js'))
-  .pipe($.uglify({ preserveComments: 'some' }))
-  // Output files
-  .pipe($.size({ title: 'scripts' }))
-  .pipe($.sourcemaps.write('.'))
-  .pipe(gulp.dest('dist/public/scripts'))
-  .pipe(gulp.dest('.tmp/scripts'))
+		// Other scripts
+	])
+	.pipe($.newer('.tmp/scripts'))
+	.pipe($.sourcemaps.init())
+	.pipe($.babel())
+	.pipe($.sourcemaps.write())
+	.pipe(gulp.dest('.tmp/scripts'))
+	.pipe($.concat('main.min.js'))
+	.pipe($.uglify({ preserveComments: 'some' }))
+	// Output files
+	.pipe($.size({ title: 'scripts' }))
+	.pipe($.sourcemaps.write('.'))
+	.pipe(gulp.dest('dist/public/scripts'))
+	.pipe(gulp.dest('.tmp/scripts'))
 );
 
 // html includes
 gulp.task('htmlIncludes', function() {
-  return gulp.src(['app/*.html', 'app/includes/*.html'])
-    .pipe(foreach(function(stream, file) {
-      var items = function() {
-        x
-      };
-      console.info(file.relative.slice(0, -5));
-      return stream
-        .pipe(preprocess())
-        .on('error', function(e) { handleError(e) });
-    }))
-    .pipe(gulp.dest('./.tmp/'))
-    .pipe(gulp.dest('dist/public'))
-    .on('error', function(e) { handleError(e) });
+	return gulp.src(['app/*.html', 'app/includes/*.html'])
+		.pipe(foreach(function(stream, file) {
+			var items = function() {
+				x
+			};
+			console.info(file.relative.slice(0, -5));
+			return stream
+				.pipe(preprocess())
+				.on('error', function(e) { handleError(e) });
+		}))
+		.pipe(gulp.dest('./.tmp/'))
+		.pipe(gulp.dest('dist/public'))
+		.on('error', function(e) { handleError(e) });
 
 });
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
-  return gulp.src('app/**/*.html')
-    .pipe(foreach(function(stream, file) {
-      var items = function() {
-        x
-      };
-      console.info(file.relative.slice(0, -5));
-      return stream
-        .pipe(preprocess())
-        .on('error', function(e) { handleError(e) });
-    }))
+	return gulp.src('app/**/*.html')
+		.pipe(foreach(function(stream, file) {
+			var items = function() {
+				x
+			};
+			console.info(file.relative.slice(0, -5));
+			return stream
+				.pipe(preprocess())
+				.on('error', function(e) { handleError(e) });
+		}))
 
-    .pipe($.useref({
-      searchPath: '{.tmp,app}',
-      noAssets: true
-    }))
+		.pipe($.useref({
+			searchPath: '{.tmp,app}',
+			noAssets: true
+		}))
 
-    // Minify any HTML
-    .pipe($.if('*.html', $.htmlmin({
-      minifyJS: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true,
-      removeAttributeQuotes: true,
-      removeRedundantAttributes: true,
-      removeEmptyAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      removeOptionalTags: true
-    })))
-    // Output files
-    .pipe($.if('*.html', $.size({ title: 'html', showFiles: true })))
-    .pipe(gulp.dest('dist/public'));
+		// Minify any HTML
+		.pipe($.if('*.html', $.htmlmin({
+			minifyJS: true,
+			removeComments: true,
+			collapseWhitespace: true,
+			collapseBooleanAttributes: true,
+			removeAttributeQuotes: true,
+			removeRedundantAttributes: true,
+			removeEmptyAttributes: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			removeOptionalTags: true
+		})))
+		// Output files
+		.pipe($.if('*.html', $.size({ title: 'html', showFiles: true })))
+		.pipe(gulp.dest('dist/public'));
 });
 
 // Clean output directory
@@ -208,100 +208,100 @@ gulp.task('clean', () => del(['.tmp', 'dist/public/*', '!dist/public/.git'], { d
 
 
 gulp.task('buildStoreItems', ['convertStoreXML', 'getStoreXML'], () => {
-  return gulp
-    .src('app/includes/store-products.tmpl', { base: "./" })
-    .pipe(data(() => (JSON.parse(
-      fs.readFileSync('docs/zazzle.json')
-    ))))
-    .pipe(template())
-    .pipe(rename({ extname: '.html' }))
-    .pipe(gulp.dest('./'))
-    .pipe(gulp.dest('dist/public'))
-    .on('error', function(e) { handleError(e) });
+	return gulp
+		.src('app/includes/store-products.tmpl', { base: "./" })
+		.pipe(data(() => (JSON.parse(
+			fs.readFileSync('docs/zazzle.json')
+		))))
+		.pipe(template())
+		.pipe(rename({ extname: '.html' }))
+		.pipe(gulp.dest('./'))
+		.pipe(gulp.dest('dist/public'))
+		.on('error', function(e) { handleError(e) });
 });
 
 
 // Watch files for changes & reload
 gulp.task('serve', ['scripts', 'styles', 'buildStoreItems', 'htmlIncludes'], () => {
-  browserSync({
-    notify: false,
-    // Customize the Browsersync console logging prefix
-    logPrefix: 'WSK',
-    // Allow scroll syncing across breakpoints
-    scrollElementMapping: ['main', '.mdl-layout'],
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    https: false,
-    server: ['.tmp', 'app'],
-    port: 3001
-  });
+	browserSync({
+		notify: false,
+		// Customize the Browsersync console logging prefix
+		logPrefix: 'WSK',
+		// Allow scroll syncing across breakpoints
+		scrollElementMapping: ['main', '.mdl-layout'],
+		// Run as an https by uncommenting 'https: true'
+		// Note: this uses an unsigned certificate which on first access
+		//       will present a certificate warning in the browser.
+		https: false,
+		server: ['.tmp', 'app'],
+		port: 3001
+	});
 
-  gulp.watch(['.tmp/**/*.html', 'app/**/*.html'], ['htmlIncludes', reload]);
-  gulp.watch(['app/includes/store-products.tmpl'], ['buildStoreItems', reload]);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js', 'app/js/**/*.js'], ['lint', 'scripts', reload]);
-  gulp.watch(['app/images/**/*'], reload);
+	gulp.watch(['.tmp/**/*.html', 'app/**/*.html'], ['htmlIncludes', reload]);
+	gulp.watch(['app/includes/store-products.tmpl'], ['buildStoreItems', reload]);
+	gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
+	gulp.watch(['app/scripts/**/*.js', 'app/js/**/*.js'], ['lint', 'scripts', reload]);
+	gulp.watch(['app/images/**/*'], reload);
 });
 
 // Build and serve the output from the dist build
 gulp.task('serve:dist', ['default'], () =>
-  browserSync({
-    notify: false,
-    logPrefix: 'WSK',
-    // Allow scroll syncing across breakpoints
-    scrollElementMapping: ['main', '.mdl-layout'],
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: 'dist',
-    port: 3002
-  })
+	browserSync({
+		notify: false,
+		logPrefix: 'WSK',
+		// Allow scroll syncing across breakpoints
+		scrollElementMapping: ['main', '.mdl-layout'],
+		// Run as an https by uncommenting 'https: true'
+		// Note: this uses an unsigned certificate which on first access
+		//       will present a certificate warning in the browser.
+		// https: true,
+		server: 'dist',
+		port: 3002
+	})
 );
 gulp.task('getStoreXML', () => {
-  console.info('getStoreXML');
-  return download({
-      file: "zazzle.xml",
-      url: 'https://feed.zazzle.com/luckyape/rss'
-    })
-    .pipe(gulp.dest('docs'))
-    .on('error', function(e) { handleError(e) });
+	console.info('getStoreXML');
+	return download({
+			file: "zazzle.xml",
+			url: 'https://feed.zazzle.com/luckyape/rss'
+		})
+		.pipe(gulp.dest('docs'))
+		.on('error', function(e) { handleError(e) });
 });
 
 gulp.task('convertStoreXML', ['getStoreXML'], () => {
-  console.info('convertStoreXML');
-  gulp.src('docs/zazzle.xml')
-    .pipe(xml2json())
-    .pipe(rename({ extname: '.json' }))
-    .pipe(gulp.dest('docs'))
-    .on('error', function(e) { handleError(e) });
+	console.info('convertStoreXML');
+	gulp.src('docs/zazzle.xml')
+		.pipe(xml2json())
+		.pipe(rename({ extname: '.json' }))
+		.pipe(gulp.dest('docs'))
+		.on('error', function(e) { handleError(e) });
 });
 
 // Build production files, the default tas
 gulp.task('default', ['clean'], cb => {    
-  runSequence(
-   'styles', ['lint', 'html', 'scripts', 'images', 'copy'],
-    cb
-  )
+	runSequence(
+	 'styles', ['lint', 'html', 'scripts', 'images', 'copy', 'copyfonts'],
+		cb
+	)
 });
 
 // Run PageSpeed Insights
 gulp.task('pagespeed', cb =>
-  // Update the below URL to the public URL of your site
-  pagespeed('luckyape.com', {
-    strategy: 'mobile'
-    // By default we use the PageSpeed Insights free (no API key) tier.
-    // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
-    // key: 'YOUR_API_KEY'
-  }, cb)
+	// Update the below URL to the public URL of your site
+	pagespeed('luckyape.com', {
+		strategy: 'mobile'
+		// By default we use the PageSpeed Insights free (no API key) tier.
+		// Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
+		// key: 'YOUR_API_KEY'
+	}, cb)
 );
 
 // Copy over the scripts that are used in importScripts as part of the generate-service-worker task.
 gulp.task('copy-sw-scripts', () => {
  
-  return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
-    .pipe(gulp.dest('app/scripts/sw'));
+	return gulp.src(['node_modules/sw-toolbox/sw-toolbox.js', 'app/scripts/sw/runtime-caching.js'])
+		.pipe(gulp.dest('app/scripts/sw'));
 });
 
 
@@ -311,30 +311,30 @@ gulp.task('copy-sw-scripts', () => {
 // local resources. This should only be done for the 'dist' directory, to allow
 // live reload to work as expected when serving from the 'app' directory.
 gulp.task('generate-service-worker', ['copy-sw-scripts'], () => {
-  const rootDir = 'dist/public';
-  const filepath = path.join(rootDir, 'service-worker.js');
-  console.info('generate-service-worker');
-  return swPrecache.write(filepath, {
-    // Used to avoid cache conflicts when serving on localhost.
-    cacheId: pkg.name || 'web-starter-kit',
-    // sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
-    importScripts: [
-      'scripts/sw/sw-toolbox.js',
-      'scripts/sw/runtime-caching.js'
-    ],
-    staticFileGlobs: [
-      // Add/remove glob patterns to match your directory setup.
-      `${rootDir}/images/**/*`,
-      `${rootDir}/scripts/**/*.js`,
-      `${rootDir}/styles/**/*.css`,
-      `${rootDir}/fonts/**/*`,
-      `${rootDir}/*.{html,json}`
-    ],
-    // Translates a static file path to the relative URL that it's served from.
-    // This is '/' rather than path.sep because the paths returned from
-    // glob always use '/'.
-    stripPrefix: rootDir + '/'
-  });
+	const rootDir = 'dist/public';
+	const filepath = path.join(rootDir, 'service-worker.js');
+	console.info('generate-service-worker');
+	return swPrecache.write(filepath, {
+		// Used to avoid cache conflicts when serving on localhost.
+		cacheId: pkg.name || 'web-starter-kit',
+		// sw-toolbox.js needs to be listed first. It sets up methods used in runtime-caching.js.
+		importScripts: [
+			'scripts/sw/sw-toolbox.js',
+			'scripts/sw/runtime-caching.js'
+		],
+		staticFileGlobs: [
+			// Add/remove glob patterns to match your directory setup.
+			`${rootDir}/images/**/*`,
+			`${rootDir}/scripts/**/*.js`,
+			`${rootDir}/styles/**/*.css`,
+			`${rootDir}/fonts/**/*`,
+			`${rootDir}/*.{html,json}`
+		],
+		// Translates a static file path to the relative URL that it's served from.
+		// This is '/' rather than path.sep because the paths returned from
+		// glob always use '/'.
+		stripPrefix: rootDir + '/'
+	});
 });
 
 // Load custom tasks from the `tasks` directory
