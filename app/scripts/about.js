a579f6aead6e974d1f609784c3011ca39a5671bc
graham charles <graham@luckyape.com>
2017-12-19 15:09:01 -0800

    refactor for deploy

---
+++ app/scripts/about.js
@@ -0,0 +1,162 @@
+
+/* exported initScroller */
+/* global  window, document, getComputedStyle, probPhone */
+	'use strict';
+	var panel = document.getElementById('panels');
+	var pHead = document.getElementById('panel-header');
+	var clouds = panel.querySelectorAll('p');
+	var pPos = null;
+	var pTPad = parseFloat(getComputedStyle(panel).getPropertyValue('padding-top'));
+	var dH = document.documentElement.clientHeight;
+	var paused = false;
+	// var scrollHeight = document.body.scrollHeight;//  window.innerHeight
+	var SPPos = 60;
+	// scroll pause position
+	/**
+	 * Animates shooting stars
+	 * @param {array} canvas - array of canvas dom objects
+	 */
+	function draw(canvas) {
+		if (canvas.getContext) {
+			var ctx = canvas.getContext('2d');
+			var tailW = canvas.width;
+			var tailH = canvas.height;
+			var i = 0;
+			var s = tailW / tailH;
+			var clear = function() {
+				ctx.clearRect(0, 0, tailW, tailH);
+			};
+
+			var tail = function() {
+				clear();
+				ctx.beginPath();
+				ctx.moveTo(i * s, i);
+				ctx.lineTo(tailW, tailH);
+				ctx.stroke();
+				if (i < tailH) {
+					setTimeout(tail, 0);
+				}
+				i += 2;
+			};
+
+			var meteor = function() {
+				clear();
+				ctx.beginPath();
+				ctx.moveTo(0, 0);
+				ctx.lineTo(i * s, i);
+				ctx.stroke();
+				if (i < tailH) {
+					setTimeout(meteor, 0);
+				} else {
+					ctx.lineWidth = 0.75;
+					ctx.strokeStyle = 'rgba(255, 255, 255, .8)';
+					i = 0;
+					setTimeout(tail, 100);
+				}
+				i++;
+			};
+			ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
+			ctx.lineWidth = 0.8;
+			ctx.lineCap = 'round';
+			meteor();
+		}
+	}
+	/*
+	 * Animates shooting stars
+	 * @param {array} canvas - array of canvas dom objects
+	 * @param {number} cL - length of canvas array
+	 */
+	var shower = function(canvas, cL) {
+		var small = (probPhone) ? 0.6 : 1;
+		var rN = ((Math.random() / 2) * small) + 0.5;
+		var rotate = Math.random() * Math.PI * 2 * 25;
+		var s = Math.random() * Math.floor(Math.random() * cL) + 1;
+		var delay = Math.random() * 4000 * s;
+		var screen = window.screen;
+		var x = (screen.width * Math.random()) / screen.width * 100;
+		var y = (screen.height * Math.random()) / screen.height * 80;
+		if (y > 60) {
+			rN *= 0.6;
+		}
+		var transform = ' translate(' + x + 'vw, ' + y + 'vh) scale(' + rN + ') rotate(' + rotate + 'deg)';
+
+		canvas.style.transform = transform;
+
+		draw(canvas);
+		setTimeout(function() {
+			shower(canvas, cL);
+			canvas.style.display = 'block';
+		}, delay);
+	};
+	/*
+	 * Initiates scrolling effect
+	 */
+	function initScroller() {
+		var length = clouds.length;
+		for (var i = 0; i < length; i++) {
+			if (clouds[i].getBoundingClientRect().bottom * (100 / dH) > 80) {
+				clouds[i].classList.add('blurred');
+			}
+		}
+		storyFade();
+	}
+
+	/*
+	 * Triggered by scroll
+	 */
+	function storyFade() {
+		var scrollY = window.scrollY;
+
+		if (!paused && scrollY > SPPos) {
+			pPos = pHead.getBoundingClientRect();
+			pHead.style.position = 'fixed';
+			pHead.style.left = pPos.left + 'px';
+			pHead.style.top = pPos.top + 'px';
+			paused = true;
+		} else if (paused) {
+			var tPos = panel.getBoundingClientRect();
+			var vh = (100 / dH);
+			var pPosH = pPos.height;
+
+			if ((tPos.y * vh * -1) + (pPosH * vh) > (pTPad * vh) - 22) {
+				pHead.style.position = 'absolute';
+				pHead.style.top = pTPad - pPosH + 'px';
+				pHead.style.left = 0;
+				paused = false;
+				pHead.classList.remove('bounce');
+			}
+
+			if (tPos.height > tPos.bottom) {
+				var unblur = panel.querySelector('p.blurred');
+				var blur = panel.querySelectorAll('p:not(.blurred)');
+				var uPos;
+				var uVh;
+				var bPos;
+				var bVh;
+				// this should just keep one array of objects and do it own iterating but no issue
+				if (blur.length) {
+					blur = blur[blur.length - 1];
+					bPos = blur.getBoundingClientRect();
+					bVh = bPos.bottom * vh;
+
+					if (bVh > 80) {
+						blur.classList.add('blurred');
+						blur = null;
+						return;
+					}
+				} else {
+					pHead.classList.add('bounce');
+				}
+
+				if (unblur) {
+					uPos = unblur.getBoundingClientRect();
+					uVh = uPos.bottom * vh;
+					if (uVh <= 80) {
+						unblur.classList.remove('blurred');
+						unblur = null;
+					}
+				}
+			}
+		}
+	}
+	initScroller();
