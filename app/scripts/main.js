5a207dc321ca797001f5bcc3dc9f2778f9961962
Graham Charles <graham@luckyape.com>
2017-12-20 01:19:56 -0800

    more refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,63 +27,63 @@
   // service worker from an insecure origin will trigger JS console errors. See
   // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
   var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-	// [::1] is the IPv6 localhost address.
-	window.location.hostname === '[::1]' ||
-	// 127.0.0.1/8 is considered localhost for IPv4.
-	window.location.hostname.match(
-	  /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-	)
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
   );
 
   if ('serviceWorker' in navigator &&
-	(window.location.protocol === 'https:' || isLocalhost)) {
-	navigator.serviceWorker.register('service-worker.js')
-	  .then(function(registration) {
-		// updatefound is fired if service-worker.js changes.
-		registration.onupdatefound = function() {
-		  // updatefound is also fired the very first time the SW is installed,
-		  // and there's no need to prompt for a reload at that point.
-		  // So check here to see if the page is already controlled,
-		  // i.e. whether there's an existing service worker.
-		  if (navigator.serviceWorker.controller) {
-			// The updatefound event implies that registration.installing is set:
-			// https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-			var installingWorker = registration.installing;
+    (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        // updatefound is fired if service-worker.js changes.
+        registration.onupdatefound = function() {
+          // updatefound is also fired the very first time the SW is installed,
+          // and there's no need to prompt for a reload at that point.
+          // So check here to see if the page is already controlled,
+          // i.e. whether there's an existing service worker.
+          if (navigator.serviceWorker.controller) {
+            // The updatefound event implies that registration.installing is set:
+            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+            var installingWorker = registration.installing;
 
-			installingWorker.onstatechange = function() {
-			  switch (installingWorker.state) {
-				case 'installed':
-				  // At this point, the old content will have been purged and the
-				  // fresh content will have been added to the cache.
-				  // It's the perfect time to display a "New content is
-				  // available; please refresh." message in the page's interface.
-				  break;
+            installingWorker.onstatechange = function() {
+              switch (installingWorker.state) {
+                case 'installed':
+                  // At this point, the old content will have been purged and the
+                  // fresh content will have been added to the cache.
+                  // It's the perfect time to display a "New content is
+                  // available; please refresh." message in the page's interface.
+                  break;
 
-				case 'redundant':
-				  throw new Error('The installing ' +
-					'service worker became redundant.');
+                case 'redundant':
+                  throw new Error('The installing ' +
+                    'service worker became redundant.');
 
-				default:
-				  // Ignore
-			  }
-			};
-		  }
-		};
-	  }).catch(function(e) {
-		console.error('Error during service worker registration:', e);
-	  });
+                default:
+                  // Ignore
+              }
+            };
+          }
+        };
+      }).catch(function(e) {
+        console.error('Error during service worker registration:', e);
+      });
   }
 })();
 var probPhone = ((/iphone|android|ie|blackberry|fennec/).test(navigator.userAgent.toLowerCase()) && 'ontouchstart' in document.documentElement);
 /*
  * Displayes obuscated contact info
  */
-var loadContactInfo = function() {
+window.loadContactInfo = function() {
   var contactMethods = document.getElementById('contact-methods');
   var contactHash = 'PGEgaHJlZj0ic21zOjEtNjA0LTM0MC03OTI1IiBjbGFzcz0ic21zLWxpbmsgaWNvbiBpY29uLWNoYXQgaWNvbi1jaGF0LWRpbXMiPlNNUzxkaXY+NjA0LTM0MC03OTI1PC9kaXY+PC9hPjxhIGhyZWY9Im1haWx0bzppbmZvQGx1Y2t5YXBlLmNvbT9zdWJqZWN0PSU1QldXVyUyMElucXVpcmUlNUQiIGNsYXNzPSJpY29uIGljb24tZW52ZWxvcGUgaWNvbi1lbnZlbG9wZS1kaW1zIj5FbWFpbDxkaXY+aW5mb0BsdWNreWFwZS5jb208L2Rpdj48L2E+PGEgaHJlZj0idGVsOjEtNjA0LTM0MC03OTI1IiBjbGFzcz0iaWNvbiBpY29uLXBob25lLWNhbGwgaWNvbi1waG9uZS1jYWxsLWRpbXMiPlRlbDxkaXY+NjA0LTM0MC03OTI1PC9kaXY+PC9hPg==';
   contactMethods.innerHTML = atob(contactHash);
   if (0 && !probPhone) {
-	var smsLink = document.querySelector('.sms-link');
-	document.querySelector('.contact-methods').removeChild(smsLink);
+    var smsLink = document.querySelector('.sms-link');
+    document.querySelector('.contact-methods').removeChild(smsLink);
   }
 };
