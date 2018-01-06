abdd6cc91aa504f649d628879f6d3c6b737a1647
Graham Charles <graham@luckyape.com>
2018-01-06 02:15:07 -0800

    add js css to compressive options

--- dist/server.js
+++ dist/server.js
@@ -22,7 +22,7 @@ app.all('*', ensureDomain);
 app.use(compression());
 
 // default to .html (you can omit the extension in the URL)
-app.use(serveStatic(`${__dirname}/public`, {'extensions': ['html']}));
+app.use(serveStatic(`${__dirname}/public`, {'extensions': ['html','js','css']}));
 
 app.listen(port, () => {
   console.log('Server running...');
